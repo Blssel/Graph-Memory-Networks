6ac3f347b77c7afc104fe746b1c7e5005f9fb4c9
yinzhiyu_sx <1109047248@qq.com>
2018-07-23 22:29:44 +0800

    new added

--- src/memory.py
+++ src/memory.py
@@ -1,34 +1,59 @@
 #coding:utf-8
+
 import numpy as np
 import tensorflow as tf
+from train import cfg
 
 class Cell:
   '''
   args:
+    feature: a tensor of shape(dim_m)
     neighbors: a 2-dim list,each line stores the ids of a specific kind of neighbors
   '''
   def __init__(self,feature,neighbors):
-    self.feature = feature
-    self.neighbors=neighbors
+    '''initialize an empty
+    :param feature:
+    :param neighbors:
+    '''
+    self.feature = tf.constant(0,dtype=tf.float32,shape=[cfgs.SHAPE])
+    self.neighbors=
 
 class GraphMemory:
   cells = [] #considering batch processing, the size should be:batch_size*cell_size
-  def __init__(self,batch_size):
-    self.batch_size = batch_size
+  '''Initialize an empty graph(or graph memory)
+  '''
+  mem=tf.placeholder(0,shape=[cfg.batch_size,None,cfg.dim_m])  #batch_size* max_nodes* dim_m
+  adj_matrix=None
+  def __init__(self,cfg):
     self.construct_mem()  #construct memory
-
+    self.mem=[[] for i in range(cfg.batch_size)]
+  '''
   def construct_mem(self):
-    '''Assign id for each node in order, then put them into corresponding memory cell.
-    '''
+    #Assign id for each node in order, then put them into corresponding memory cell.
     # instantiate a Cell and append to cells
-
+  '''
 
 class Controller:
-  """each time, the controller process a batch of data
-  """
+  '''each time, the controller process a batch of data
+  controller_state:shape=[batch_size,]
+  '''
   def __init__(self):
+    self.controller_state = []  # controller_state is a 2-dim tuple
 
-  def read(self):
-    #
+  def queryInput(self,cfg,query):
+    self.controller_state.append(
+      tf.contrib.layers.fully_connected(query, cfg.DIM_STATE))  # define and intialize the controller state
+    self.controller_state.append(None)
+
+  def __attention(self,memory):
+    a = tf.reshape(tf.contrib.layers.fully_connected(memory.mem, cfg.dim_a, biase=None, biases_initializer=None), [cfg.BATCH_SIZE, None]) + \
+        tf.contrib.layers.fully_connected(self.controller_state, cfg.dim_a) # a shape=[batch_size*]
+    p = tf.nn.softmax(tf.reshape(tf.contrib.layers.fully_connected(a,1,)
+                                 ,[cfg.BATCH_SIZE, graphmem.mem.get_shape().as_lsit()]))
+    m = tf.
+  def read(self,memory):
+    # attention
+
+
   def write(self):
 
--- src/train.py
+++ src/train.py
@@ -7,7 +7,13 @@ import time
 import tensorflow as tf
 import numpy as np
 import os.path as osp
-slim = tf.contrib.slim
+import dataset_factory.i3d_data_reader as reader
+sys.path.append(os.getcwd())
+from nets import i3d
+from config import cfg,cfg_from_file ,get_output_dir
+from loss import tsn_loss
+from utils.view_ckpt import view_ckp
+from memory import Controller
 
 def average_gradients(tower_grads):
   average_grads=[]
@@ -30,9 +36,42 @@ def _parse_args():
   args=parser.parse_args()
   return args
 
+def repeat(v,times):
+  return np.repeat(v,times,2)
 def main():
-  return 0
-'''
-if __name__=='__main__':
-  main()
-'''
+  #-------------parse arguments-------------#
+  args=_parse_args()
+  if args.cfg_file is not None:
+    cfg_from_file(args.cfg_file)  #读取args.cfg_file文件内容并融合到cfg中
+  pprint.pprint(cfg)
+
+  #-------------some configurations-------------#
+  os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+  os.environ['CUDA_VISBLE_DEVICES']= cfg.GPUS
+  tf.logging.set_verbosity(tf.logging.INFO) #设置日志级别
+
+  # -------------build the graph-------------#
+  controller = Controller()
+  with tf.VariableScope("Query_input"):
+    query = tf.one_hot([cfg.TASK_NUM - 1] * cfg.BATCH_SIZE, depth=cfg.TASK_NUM)[0]  # generate corresponding query,shape=[cfg.BATCH_SIZE,cfg.TASK_NUM]
+    controller.queryInput(cfg, query)
+  with tf.VariableScope("Read_Write"):
+    # get m_t
+    for i in range(cfg.NUM_ITE):
+      with tf.VariableScope("Read_%d"%i):
+        controller.read()
+
+        # attention
+        a=tf.reshape(tf.contrib.layers.fully_connected(graphmem.mem,cfg.dim_a,biase=None,biases_initializer=None), [cfg.BATCH_SIZE,None])
+          +tf.contrib.layers.fully_connected(controller_state,cfg.dim_a)
+        p=tf.nn.softmax(tf.reshape(tf.contrib.layers.fully_connected(a,1,)
+                                   , [cfg.BATCH_SIZE, graphmem.mem.get_shape().as_lsit()]))
+        m=tf
+
+      with tf.VariableScope("Write_%d"%i):
+
+
+
+
+
+        controller_state=tf.contrib.layers.fully_connected()
